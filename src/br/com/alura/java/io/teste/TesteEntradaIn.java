c2124d4bf90d92559a3c82b76eab819925d5b2bc
mateuslph <mlptche@gmail.com>
2022-09-16 22:29:02 -0300

    keyboard input and output

---
+++ src/br/com/alura/java/io/teste/TesteEntradaIn.java
@@ -0,0 +1,39 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class TesteEntradaIn {
+
+	public static void main(String[] args) throws IOException {
+
+		//fluxo de entrada com arquivo
+		InputStream fis = System.in; //= new FileInputStream("loren.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		FileOutputStream fos = new FileOutputStream("loren3.txt"); // = System.out; // saida no console
+		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha = br.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			bw.write(linha);
+			bw.newLine();
+			bw.flush();  // descarga de dados
+			linha = br.readLine();
+		}
+		
+		br.close();
+		bw.close();
+		
+	}
+
+}
