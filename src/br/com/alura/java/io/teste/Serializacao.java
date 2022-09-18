cd3674925bed6b33cf9138e80fc9b094ecac9275
mateuslph <mlptche@gmail.com>
2022-09-17 23:10:48 -0300

    added explanations

--- src/br/com/alura/java/io/teste/Serializacao.java
+++ src/br/com/alura/java/io/teste/Serializacao.java
@@ -11,6 +11,13 @@ public class Serializacao {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
 		// desserializacao
+		// se for um objeto deve implementar Serializable na classe do objeto. Em seguida sera 
+		// gerada uma identificacao para classe chamada de serialVersionUID (private static final 
+		// long serialVersionUID = 1L;), e esse numero  devera ser o mesmo da criacao do objeto. 
+		// Este numero define a mudanca da versao em modificacoes nao compativeis, como, mudar um atributo. 
+		// Todas classes com agregacao devem implementar ou colocar no atributo classe depois do tipo
+		// a palavra tramsient, que diz que essa agregacao nao precisa ser serializable
+		
 //		String nome = "Mateus Pereira";
 //		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
 //		oos.writeObject(nome);
